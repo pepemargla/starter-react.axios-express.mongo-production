1af45a610c43807fd5ca14a64d284e55daa59c4e
Pepe Martinez <pepe.martinez@korall.io>
2022-10-22 02:43:02 +0200

    Build production testing connection cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const mongoose=require('mongoose');
 const bodyParser=require('body-parser');
+const cors = require('cors')
 
 require('dotenv').config({path: './.env.development'});
 
@@ -14,11 +15,13 @@ mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-reac
   console.log("databaseconnected")
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
+app.use(cors())
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use(bodyParser.json());
 
