39c103e25ada1ed48372ef39ccb4e2ab09cc0b0c
Pepe Martinez <pepe.martinez@korall.io>
2022-10-22 02:36:37 +0200

    Build production testing connection 3

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-reac
   console.log("databaseconnected")
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 
 console.log('public url: '+process.env.PUBLIC_URL);
